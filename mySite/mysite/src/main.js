ff96f340f85225e5deb826094d2ab866a9b190f2
MarvinBrereton-creator <56225640+MarvinBrereton-creator@users.noreply.github.com>
2019-11-15 14:35:40 +0000

    Login project finalized. Moved files around.

--- mySite/mysite/src/main.js
+++ mySite/mysite/src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
-Vue.config.productionTip = false
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  1000);
+
